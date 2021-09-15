2acdc451a303ce26f729b78b3d5f5a35aacc3cd3
Brian Axel <90643264+A01753036@users.noreply.github.com>
2021-09-15 15:41:17 -0500

    Update Memorama-base.py
    Contador de taps

--- Memorama-base.py
+++ Memorama-base.py
@@ -19,7 +19,7 @@ car = path('car.gif')
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
-
+contador=0
 
 def square(x, y):
     "Draw white square with black outline at (x, y)."
@@ -48,6 +48,9 @@ def tap(x, y):
     "Update mark and hidden tiles based on tap."
     spot = index(x, y)
     mark = state['mark']
+    global contador
+    contador = contador + 1
+    print("numero de Taps " + str(contador)) 
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
